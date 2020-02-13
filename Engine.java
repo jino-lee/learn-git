5dc53db56538d6c03db7f5b3980b046dd761bf0d
Jin Lee <jinolemo@gmail.com>
2020-02-13 13:09:51 -0600

    playerTurn and checkTie implementation
    EE 422C 2/13/2020 recitation

--- Engine.java
+++ Engine.java
@@ -52,13 +52,18 @@ public class Engine {
   // TODO: Complete this method
   // Player is either 0 or 1, row and column are valid and empty
   public void playerTurn(int player, int row, int col) {
-    return;
+    board[row][col] = symbols[player];
   }
 
   // TODO: Complete this method
   // Return true if the board is full
   // Return false if not
   public boolean checkTie() {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+      for (int col = 0; col < BOARD_SIZE; col++) {
+        if(board[row][col] == " ") return false; // there is an empty space
+      }
+    }
     return true;
   }
 
